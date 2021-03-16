324f13f0d87106fbcf8542ab8955c2d766719625
Ryan Henderson <ryan.ev.henderson@gmail.com>
2021-03-16 15:39:39 -0500

    Add hide and show functions for 'other' text box

---
+++ js/site.js
@@ -0,0 +1,11 @@
+function hideOther() {
+    document.getElementById("other-specify").style.display = "none";
+    document.getElementById("other-specify-label").style.display = "none";
+}
+
+function showOther() {
+    document.getElementById("other-specify").style.display = "block";
+    document.getElementById("other-specify-label").style.display = "block";
+}
+
+hideOther();
